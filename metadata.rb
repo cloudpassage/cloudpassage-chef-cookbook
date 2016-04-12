cfaf61ad6d6a9ed2a77e14b84bcb3a9b3a4d9eb3
Ash Wilson <ash.d.wilson@gmail.com>
2016-04-12 16:31:51 -0700

    updating changelog and metadata

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ description 'Installs/Configures CloudPassage Halo'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 issues_url 'https://support.cloudpassage.com'
 source_url 'https://github.com/cloudpassage/cloudpassage-chef-cookbook'
-version '2.5.0'
+version '2.6.0'
 depends 'apt', '~>2.9.0'
 depends 'yum', '~>3.8.0'
 depends 'compat_resource'
